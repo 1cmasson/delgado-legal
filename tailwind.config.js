e76ff22b523fe37d079241f408bbd302493d486a
Carlos Masson <carlosmasson96@gmail.com>
2023-09-17 00:03:23 -0400

    hero setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,71 +1,23 @@
 // @ts-check
-const { fontFamily } = require('tailwindcss/defaultTheme')
-const colors = require('tailwindcss/colors')
 
 /** @type {import("tailwindcss/types").Config } */
 module.exports = {
   content: [
-    './node_modules/pliny/**/*.js',
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,tsx}',
-    './components/**/*.{js,ts,tsx}',
-    './layouts/**/*.{js,ts,tsx}',
-    './data/**/*.mdx',
+    './src/app/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,tsx}',
   ],
-  darkMode: 'class',
   theme: {
     extend: {
-      lineHeight: {
-        11: '2.75rem',
-        12: '3rem',
-        13: '3.25rem',
-        14: '3.5rem',
+      backgroundColor: {
+        primary:"#033A5B",
+        hover:"#E4BE72"
       },
-      fontFamily: {
-        sans: ['var(--font-space-grotesk)', ...fontFamily.sans],
-      },
-      colors: {
-        primary: colors.pink,
-        gray: colors.gray,
-      },
-      typography: ({ theme }) => ({
-        DEFAULT: {
-          css: {
-            a: {
-              color: theme('colors.primary.500'),
-              '&:hover': {
-                color: `${theme('colors.primary.600')}`,
-              },
-              code: { color: theme('colors.primary.400') },
-            },
-            'h1,h2': {
-              fontWeight: '700',
-              letterSpacing: theme('letterSpacing.tight'),
-            },
-            h3: {
-              fontWeight: '600',
-            },
-            code: {
-              color: theme('colors.indigo.500'),
-            },
-          },
-        },
-        invert: {
-          css: {
-            a: {
-              color: theme('colors.primary.500'),
-              '&:hover': {
-                color: `${theme('colors.primary.400')}`,
-              },
-              code: { color: theme('colors.primary.400') },
-            },
-            'h1,h2,h3,h4,h5,h6': {
-              color: theme('colors.gray.100'),
-            },
-          },
-        },
-      }),
+      textColor: {
+        primary:"#8B8B8B",
+        hover:"#E4BE72"
+      }
     },
   },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+
+  plugins: [require('@tailwindcss/forms')],
 }
